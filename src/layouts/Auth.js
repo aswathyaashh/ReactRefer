47f567ea71de1f2de44b41249791e04b677e9f49
alistairdsouza24 <alistairtutu@gmail.com>
2022-11-15 07:52:44 +0530

    the login page of the project

---
+++ src/layouts/Auth.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+// components
+// views
+
+import Login from "views/auth/Login.js";
+import Register from "views/auth/Register.js";
+import background from "assets/img/register_bg_2.png";
+export default function Auth() {
+  return (
+    <>
+      <main>
+        <section className="relative w-full h-full py-40 min-h-screen">
+          <div
+            className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+            style={{
+              backgroundImage:
+                `url(${background})`,
+            }}
+          ></div>
+          <Switch>
+            <Route path="/auth/login" exact component={Login} />
+            <Route path="/auth/register" exact component={Register} />
+            <Redirect from="/" to="/auth/login" />
+          </Switch>
+       
+        </section>
+      </main>
+    </>
+  );
+}
